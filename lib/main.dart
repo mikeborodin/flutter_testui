ff4909eadfb5be8b79125d16a39195ce0c9171ab
mikeborodin <mikeborodin@users.noreply.github.com>
2025-10-07 08:18:34 +0200

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,219 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:args/args.dart';
+import 'package:consola/consola.dart';
+import 'package:hotreloader/hotreloader.dart';
+import 'package:nocterm/nocterm.dart';
 import 'package:testui3/app_state.dart';
 import 'package:testui3/tests/test_event_mapper.dart';
-import 'package:testui3/ui/draw.dart';
-import 'package:args/args.dart';
+import 'package:testui3/tests/test_runner.dart';
+import 'package:testui3/tests/test_state.dart';
+import 'package:testui3/tree.dart';
+import 'package:testui3/ui/icons.dart';
 
-import 'terminal/terminal.dart';
-import 'tests/test_runner.dart';
+void main(List<String> args) async {
+  HotReloader? reloader;
 
-void main(List<String> arguments) async {
+  if (bool.fromEnvironment('hot')) {
+    reloader = await HotReloader.create();
+  }
+
   final parser = ArgParser()..addOption('command', abbr: 'c', defaultsTo: 'dart test -r json');
-  final argResults = parser.parse(arguments);
+  final argResults = parser.parse(args);
   final testCommand = argResults['command'] as String;
 
-  final testRunner = TestRunner(testCommand);
+  await runApp(App(runner: TestRunner(testCommand)));
 
-  try {
-    final terminal = Terminal()..setup();
-    terminal.write('\x1B[?25l'); // Hide the cursor using escape sequence
-    final state = AppState();
-
-    final eventProcessor = TestEventMapper(state);
-
-    final runnerSub = testRunner.stream.listen((event) {
-      eventProcessor.process(event);
-      draw(terminal, state);
-    });
-
-    testRunner.runAll();
-
-    if (!stdout.supportsAnsiEscapes) {
-      terminal.write('ANSI escaped codes are not supported');
-    }
-
-    // final timer = Timer.periodic(Duration(milliseconds: 200), (_) {
-    // draw(terminal, state);
-    // });
-
-    final sub = terminal.readKeys().listen((keyEvent) {
-      if (keyEvent.character == 's') {
-        testRunner.stopAll();
-      }
-
-      if (keyEvent.character == 'r') {
-        // testRunner.runAll();
-        draw(terminal, state);
-      }
-
-      if (keyEvent.character == 'q') {
-        exitApp(terminal, null);
-      }
-
-      if (keyEvent.character == 'e') {
-        state.index = state.index + 1;
-        state.statusLine += keyEvent.character ?? '';
-        // console.write('hello');
-        draw(terminal, state);
-      }
-
-      if (keyEvent.character == 'u') {
-        state.index = state.index - 1;
-        state.statusLine += keyEvent.character ?? '';
-        draw(terminal, state);
-      }
-
-      if (keyEvent.character == 'q') {
-        exitApp(terminal, null);
-      }
-
-      if (keyEvent.character == 'r') {
-        testRunner.runAll();
-      }
-
-      state.statusLine = keyEvent.toString();
-    });
-
-    await Future.any([ProcessSignal.sigint.watch().first, sub.asFuture(), runnerSub.asFuture()]);
-
-    await sub.cancel();
-    await runnerSub.cancel();
-    // timer.cancel();
-    exitApp(terminal, null);
-  } catch (err) {
-    // console.showCursor();
-    // exitApp(console, err.toString());
-  }
+  reloader?.stop();
 }
 
-void exitApp(Terminal terminal, String? errorMessage) {
-  // console.;
-  // console.rawMode = false;
-  // console.resetCursorPosition();
+class App extends StatefulComponent {
+  final TestRunner runner;
+  const App({super.key, required this.runner});
 
-  terminal.write('\x1B[?25h'); // Show the cursor using escape sequence
-  if (errorMessage != null) {
-    terminal.write(errorMessage);
-    exit(1);
-  } else {
-    exit(0);
+  @override
+  State<App> createState() => _AppState(runner: runner);
+}
+
+class _AppState extends State<App> {
+  TestRunner runner;
+  _AppState({required this.runner});
+
+  final AppState state = AppState();
+  StreamSubscription<dynamic>? sub;
+
+  int position = 0;
+  final scrollController = ScrollController();
+  bool detailsVisible = false;
+
+  TreeNode? testTreeState;
+  TreeNode? selectedNode;
+
+  @override
+  void initState() {
+    final eventProcessor = TestEventMapper(state);
+
+    runner.runAll();
+    setState(() {
+      state.statusLine = 'starting';
+    });
+
+    sub = runner.stream.listen((event) {
+      setState(() {
+        state.statusLine = event.toString();
+        eventProcessor.process(event);
+
+        testTreeState = state.tree != null
+            ? buildTree(state.tree!)
+            : TreeNode(child: Text('Test tree loading...'), children: []);
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    sub?.cancel();
+    super.dispose();
+  }
+
+  void _handleKeyEvent(KeyboardEvent event) {
+    // final height = Console.getWindowHeight();
+    setState(() {
+      if (event.character == 'q') {
+        stdout.write('${ConsoleStrings.eraseScreen}${ConsoleStrings.cursorToHome}');
+        exit(0);
+      }
+
+      if (event.character == 'r') {
+        runner.runAll();
+      }
+      if (event.character == 'p') {
+        detailsVisible = !detailsVisible;
+      }
+
+      // if (event.character == 'u') {
+      //   if (position > 0) {
+      //     position--;
+      //
+      //     if (position == scrollController.offset - 1) {
+      //       scrollController.scrollUp();
+      //     }
+      //   }
+      // }
+      //
+      // if (event.character == 'e') {
+      //   if (position + 1 < state.testsList.length) {
+      //     position++;
+      //
+      //     if (position >= height - 1) {
+      //       scrollController.scrollDown();
+      //     }
+      //   }
+      // }
+      //
+      if (event.logicalKey == LogicalKey.escape || event.matches(LogicalKey.keyQ)) {
+        exit(0);
+      }
+    });
+  }
+
+  @override
+  Component build(BuildContext context) {
+    // final width = Console.getWindowWidth();
+    return Focusable(
+      focused: true,
+      onKeyEvent: (event) {
+        setState(() {
+          if (event.character == 'q') {
+            stdout.write('${ConsoleStrings.eraseScreen}${ConsoleStrings.cursorToHome}');
+            exit(0);
+          }
+
+          if (event.character == 'r') {
+            runner.runAll();
+          }
+          if (event.character == 'p') {
+            detailsVisible = !detailsVisible;
+          }
+        });
+
+        return true;
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: testTreeState != null
+                      ? Container(
+                          color: Colors.black,
+                          padding: EdgeInsets.all(2),
+                          child: Tree(
+                            controller: scrollController,
+                            data: testTreeState!,
+                            onSelected: (v) {
+                              setState(() {
+                                selectedNode = v;
+                                detailsVisible = true;
+                              });
+                            },
+                          ),
+                        )
+                      : Text('loading...'),
+                ),
+                if (detailsVisible && testTreeState != null) VerticalDivider(),
+                if (detailsVisible && testTreeState != null)
+                  Container(
+                    width: 60,
+                    color: Colors.black,
+                    padding: EdgeInsets.all(2),
+                    child: Text(selectedNode?.children.length.toString() ?? 'nothing'),
+                  ),
+              ],
+            ),
+          ),
+          Container(color: Colors.black, child: Text(state.statusLine)),
+        ],
+      ),
+    );
+  }
+
+  TreeNode buildTree(TestTreeData root) {
+    String icon(TestState testState) => testState.result == 'success'
+        ? Icons.check
+        : testState.result == 'running'
+        ? Icons.inProgress
+        : Icons.error;
+
+    Color? color(TestState testState) => testState.result == 'success'
+        ? Colors.green
+        : testState.result == 'running'
+        ? null
+        : Colors.red;
+
+    String name = '';
+    if (root.state?.name != null) {
+      name = ' ${icon(root.state!)} ${root.state!.name}';
+    } else if (root.testFile != null) {
+      name = root.testFile!;
+    }
+
+    return TreeNode(
+      child: name.isNotEmpty
+          ? Text(
+              name,
+              maxLines: 1,
+              style: TextStyle(color: root.state != null ? color(root.state!) : null),
+            )
+          : Divider(),
+      children: [for (final child in root.children) buildTree(child)],
+    );
   }
 }
